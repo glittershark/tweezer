006a01d01cf4bb9c71000be113b78c1b2392ffba
Griffin Smith <wildgriffin45@gmail.com>
2015-09-20 13:31:44 -0400

    Add ability to initialize a gem with a name

--- lib/tweezer/gem.rb
+++ lib/tweezer/gem.rb
@@ -1,7 +1,11 @@
 module Tweezer
   class Gem
-    def initialize(ast)
-      @name = ast.children[2].children[0]
+    def initialize(node_or_name)
+      if node_or_name.is_a? Parser::AST::Node
+        @name = node_or_name.children[2].children[0]
+      else
+        @name = node_or_name
+      end
     end
 
     attr_reader :name
--- spec/tweezer/gem_spec.rb
+++ spec/tweezer/gem_spec.rb
@@ -3,8 +3,15 @@ require 'parser/current'
 
 describe Tweezer::Gem do
   describe '#initialize' do
-    let(:ast) { Parser::CurrentRuby.parse('gem "test"') }
-    subject { described_class.new(ast) }
-    its(:name) { is_expected.to eq 'test' }
+    context 'with an AST node' do
+      let(:node) { Parser::CurrentRuby.parse('gem "test"') }
+      subject { described_class.new(node) }
+      its(:name) { is_expected.to eq 'test' }
+    end
+
+    context 'with a name' do
+      subject { described_class.new('tweezer') }
+      its(:name) { is_expected.to eq 'tweezer' }
+    end
   end
 end
