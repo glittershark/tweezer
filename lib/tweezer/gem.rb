48e109e057ec2543f7076d8c0577cde0ff27d975
Griffin Smith <wildgriffin45@gmail.com>
2015-09-20 13:39:59 -0400

    Add versions to gems
    Pull out versions from gems, either directly passed to .new or from the node

--- lib/tweezer/gem.rb
+++ lib/tweezer/gem.rb
@@ -1,13 +1,25 @@
 module Tweezer
   class Gem
-    def initialize(node_or_name)
+    def initialize(node_or_name, version = nil)
       if node_or_name.is_a? Parser::AST::Node
-        @name = node_or_name.children[2].children[0]
+        check_node!(node_or_name)
+
+        arguments = node_or_name.children[2..-1]
+
+        @name = arguments[0].children[0]
+        @version = arguments[1].children[0] if arguments[1]
       else
         @name = node_or_name
+        @version = version
       end
     end
 
-    attr_reader :name
+    attr_reader :name, :version
+
+    private
+
+    def check_node!(node)
+      fail ArgumentError, 'Not a call to `gem`' if node.children[1] != :gem
+    end
   end
 end
--- spec/tweezer/gem_spec.rb
+++ spec/tweezer/gem_spec.rb
@@ -4,14 +4,29 @@ require 'parser/current'
 describe Tweezer::Gem do
   describe '#initialize' do
     context 'with an AST node' do
-      let(:node) { Parser::CurrentRuby.parse('gem "test"') }
-      subject { described_class.new(node) }
-      its(:name) { is_expected.to eq 'test' }
+      context 'with just a name' do
+        let(:node) { Parser::CurrentRuby.parse('gem "test"') }
+        subject { described_class.new(node) }
+        its(:name) { is_expected.to eq 'test' }
+      end
+
+      context 'with a version' do
+        let(:node) { Parser::CurrentRuby.parse('gem "test", "~> 1.0.0"') }
+        subject { described_class.new(node) }
+        its(:name) { is_expected.to eq 'test' }
+        its(:version) { is_expected.to eq '~> 1.0.0' }
+      end
     end
 
     context 'with a name' do
       subject { described_class.new('tweezer') }
       its(:name) { is_expected.to eq 'tweezer' }
+    end
+
+    context 'with a name and a version' do
+      subject { described_class.new('tweezer', '~> 1.0.0') }
+      its(:name) { is_expected.to eq 'tweezer' }
+      its(:version) { is_expected.to eq '~> 1.0.0' }
     end
   end
 end
